7a5a94513f653d7e3646a6d0698b8903fda634b2
Sid's Windows <1906211@kiit.ac.in>
2022-09-08 16:10:10 +0530

    assistant wishing

--- main.py
+++ main.py
@@ -6,12 +6,22 @@ import webbrowser
 import os
 import smtplib 
 
-def speakBaby():
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour in range(0,12):
+        speakBaby("Good morning Sid!")
+    elif hour in range(12,16):
+        speakBaby("Good Afternoon Sid!")
+    else:
+        speakBaby("Good Evening Sid!")        
+
+def speakBaby(audio):
     engine = pyttsx3.init('sapi5')
     voices = engine.getProperty('voices')
-    for voice in voices:
-        engine.setProperty('voice', voice.id)
-        engine.say('The quick brown fox jumped over the lazy dog.')
+    engine.setProperty('voice', voices[1].id)
+    engine.say(audio)
     engine.runAndWait()
 
-speakBaby()    
+speakBaby("Initialized...")
+wishMe()    
+
